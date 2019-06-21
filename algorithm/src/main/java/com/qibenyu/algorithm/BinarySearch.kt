00dab33bfae2bb3bc54b0d0c55effe476d58c989
qibenyu <qibenyu@cmcm.com>
2019-06-21 13:27:45 +0800

    添加introduce 页面
    Signed-off-by: qibenyu <qibenyu@cmcm.com>

---
+++ algorithm/src/main/java/com/qibenyu/algorithm/BinarySearch.kt
@@ -0,0 +1,16 @@
+package com.qibenyu.algorithm
+
+
+/**
+ * 二分查找
+ */
+fun binarySearch(array: IntArray, target: Int) {
+
+    for (i in array.indices) {
+
+        
+
+    }
+
+
+}
--- app/src/main/java/com/qibenyu/interview/resume/IntroduceActivity.kt
+++ app/src/main/java/com/qibenyu/interview/resume/IntroduceActivity.kt
@@ -1,8 +1,24 @@
 package com.qibenyu.interview.resume
 
 import android.app.Activity
+import android.os.Bundle
+import com.qibenyu.interview.R
+import kotlinx.android.synthetic.main.activity_introduce.*
+import org.markdown4j.Markdown4jProcessor
 
 
 class IntroduceActivity : Activity() {
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_introduce)
+
+        val input = resources.openRawResource(R.raw.resume)
+
+        val html = Markdown4jProcessor().process(input)
+
+        mWebView.loadDataWithBaseURL(null, html, "text/html", "utf-8", null)
+
+    }
 }
