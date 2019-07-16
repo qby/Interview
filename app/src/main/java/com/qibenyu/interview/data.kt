0be32535e4f225df038104697960cab03272b44a
qibenyu <qibenyu@cmcm.com>
2019-07-16 13:08:05 +0800

    add jetpack module
    Signed-off-by: qibenyu <qibenyu@cmcm.com>

--- app/src/main/java/com/qibenyu/interview/data.kt
+++ app/src/main/java/com/qibenyu/interview/data.kt
@@ -2,6 +2,7 @@ package com.qibenyu.interview
 
 import android.app.Activity
 import com.qibenyu.algorithm.AlgorithmActivity
+import com.qibenyu.architecture.jetpack.JetpackActivity
 import com.qibenyu.ui.LoadingActivity
 
 
@@ -17,7 +18,9 @@ var algorithmMap = hashMapOf<String, Class<out Activity>>(
     "Binary Search" to AlgorithmActivity::class.java
 )
 
-var architectureMap = hashMapOf<String, Class<out Activity>>()
+var architectureMap = hashMapOf<String, Class<out Activity>>(
+    "Jetpack" to JetpackActivity::class.java
+)
 
 var summaryMap = hashMapOf<String, Class<out Activity>>()
 
---
+++ architecture/src/main/java/com/qibenyu/architecture/jetpack/JetpackActivity.kt
@@ -0,0 +1,13 @@
+package com.qibenyu.architecture.jetpack
+
+import android.app.Activity
+import android.os.Bundle
+
+
+class JetpackActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
